2dd62e7eb2f634d377b6a20b694a78d3171892a1
Chukreev Alexey <chukreev.alexey@gmail.com>
2016-01-19 12:10:07 +0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='django-fastcgi',
     version='0.1',
-    description='Netangels django-fastcgi package',
+    description="Package for deploying django projects via FastCGI for django version 1.9 or higher.",
     author='NetAngels team',
     author_email='info@netangels.ru',
-    url='https://www.netangels.ru',
-    packages=['django_fastcgi', ],
+    packages=find_packages(),
+    keywords="django fastcgi",
+    url="https://github.com/NetAngels/django-fastcgi",
 )
